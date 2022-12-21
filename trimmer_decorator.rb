e115cadb2ff6a4c585b8182504c92b85aade7647
Daron976 <daronkwiafe@gmail.com>
2022-12-21 07:56:41 +0200

    make changes

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,14 +2,6 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @nameable.correct_name unless @nameable.correct_name.size > 10
-    stripped = @nameable.correct_name.strip
-    if stripped.size > 10
-      split_arr = @nameable.correct_name.chars
-      split_arr.pop while split_arr.length > 10
-      split_arr.join
-    else
-      stripped
-    end
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
