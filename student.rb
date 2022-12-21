0cf997d8e0d2def3891cef4a10727b5327f0e05d
Daron976 <daronkwiafe@gmail.com>
2022-12-21 17:43:55 +0200

    Make required changes

--- student.rb
+++ student.rb
@@ -14,6 +14,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.student.includes?(self)
+    classroom.students.push(self) unless classroom.students.includes?(self)
   end
 end
