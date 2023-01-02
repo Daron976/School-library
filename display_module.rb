3d64e6c1a1ecf4f624fe28dc50e71a6310d1a90c
Daron976 <daronkwiafe@gmail.com>
2023-01-02 14:02:41 +0200

    Complete refactor

--- display_module.rb
+++ display_module.rb
@@ -1,32 +1,21 @@
 module DisplayModule
-  def display_available(array, class_instance, array_name)
+  def display_available(array, array_name)
     puts "there are no #{array_name} available" if array.length.zero?
-    class_instance.message(array)
+    case array_name
+    when 'persons' then
+      array.each do |el|
+        puts "[(#{el.class}) | id: #{el.id} | name: #{el.name} | age: #{el.age}]"
+      end
+    when 'books'
+      array.each do |el|
+        puts "[title: #{el.title} | author: #{el.author}]"
+      end
+    when 'rentals'
+      array.each do |el|
+        puts "[date: #{el.date} | person: #{el.person.name} | book: #{el.book.title}]"
+      end
+    end
     puts ''
     prompt
-  end
-end
-
-class PersonItems
-  def message(array)
-    array.each do |el|
-      puts "[(#{el.class}) | id: #{el.id} | name: #{el.name} | age: #{el.age}]"
-    end
-  end
-end
-
-class BookItems
-  def message(array)
-    array.each do |el|
-      puts "[title: #{el.title} | author: #{el.author}]"
-    end
-  end
-end
-
-class RentalItems
-  def message(array)
-    array.each do |el|
-      puts "[date: #{el.date} | person: #{el.person.name} | book: #{el.book.title}]"
-    end
   end
 end
